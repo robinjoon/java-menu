df91ef7fb993901cf9b75cf3b8c2ab9e7f3b6b27
dong_hy <kimadonghyon99@gmail.com>
2023-11-30 15:53:47 +0900

    feat : Week 카테고리 중복갯수 수정

--- src/main/java/menu/Week.java
+++ src/main/java/menu/Week.java
@@ -9,12 +9,12 @@ public class Week {
     private static Week week;
     private static final List<Menu> Categories = new ArrayList<>();
     private static int size = 5;
-
+    private static int sameCategory = 2;
     private Week() {
         while (Categories.size() < size) {
             Menu category = Menu.getCategory(Randoms.pickNumberInRange(1, 5));
             int count = Collections.frequency(Categories, category);
-            if (count > 2) {
+            if (count > sameCategory-1) {
                 continue;
             }
             Categories.add(category);
@@ -31,5 +31,17 @@ public class Week {
     public List<Menu> getCategories() {
         getInstance();
         return this.Categories;
+    }
+    private String getCategoryOfDay(){
+        StringBuilder sb = new StringBuilder();
+        Categories.stream().forEach(category->{
+            sb.append(" | ");
+            sb.append(category.getCategoryName() );
+        });
+        return sb.toString();
+    }
+    @Override
+    public String toString(){
+        return "[ 카테고리%s ]".formatted(getCategoryOfDay());
     }
 }
