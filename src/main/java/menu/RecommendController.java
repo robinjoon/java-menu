01468ce16b2a0c8f117619b66d7a0e7b0bf3e1d8
dong_hy <kimadonghyon99@gmail.com>
2023-11-30 17:15:02 +0900

    refactor : 메서드 분리

--- src/main/java/menu/RecommendController.java
+++ src/main/java/menu/RecommendController.java
@@ -9,28 +9,25 @@ import menu.view.OutputView;
 public class RecommendController {
     public static void start(){
         OutputView.recommendMenuStart();
-        String coachNames = InputManger.inputCoachName();
-        List<Coach> Coaches = new ArrayList<>();
-        Arrays.stream(coachNames.split(",")).forEach(name->{
-            Coaches.add(new Coach(name,InputManger.inputPickyMenu(name)));
-        });
-
+        Coaches coaches = new Coaches(InputManger.inputCoachName());
+        printOption();
+        recommendMenu(coaches);
+        coaches.getCoaches().forEach(System.out::println);
+        OutputView.recommendFinish();
+    }
+    private static void printOption(){
         OutputView.printResult();
         OutputView.printGubn();
         OutputView.printCategory(Week.getInstance().toString());
-        recommendMenu(Coaches);
-        Coaches.stream().forEach(coach -> System.out.println(coach));
-        OutputView.recommendFinish();
     }
-
-    private static void recommendMenu(List<Coach> coaches){
+    private static void recommendMenu(Coaches coaches){
         Week.getInstance().getCategories().forEach(category->{
             recommend(category,coaches);
         });
     }
 
-    private static void recommend(Menu category, List<Coach> coaches){
-        coaches.stream().forEach(coach -> {
+    private static void recommend(Menu category, Coaches coaches){
+        coaches.getCoaches().stream().forEach(coach -> {
             coach.setRecommendedMenu(category);
         });
     }
