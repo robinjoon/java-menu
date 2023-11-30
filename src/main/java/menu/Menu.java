ad5cc8b2f6afe99059263fa9343cf7371882fad2
dong_hy <kimadonghyon99@gmail.com>
2023-11-30 17:14:25 +0900

    feat : 가지고있는모든메뉴에서 입력받은 메뉴가 존재하는지 확인하는 메서드 추가

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,21 +1,22 @@
 package menu;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public enum Menu {
-    JAPANESE("일식",1, Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
-    KOREAN("한식",2, Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
-    CHINESE("중식",3, Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN("아시안",4, Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
-    FRENCH("양식",5, Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")),
+    JAPANESE("일식", 1, Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN("한식", 2, Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE("중식", 3, Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN("아시안", 4, Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    FRENCH("양식", 5, Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")),
     ;
     private final String categoryName;
     private final int categoryNumber;
     private final List<String> menus;
 
-    Menu(String categoryName,int categoryNumber, List<String> list) {
+    Menu(String categoryName, int categoryNumber, List<String> list) {
         this.categoryName = categoryName;
         this.categoryNumber = categoryNumber;
         this.menus = list;
@@ -29,7 +30,7 @@ public enum Menu {
         return categoryName;
     }
 
-    public static String getMenu(Menu menu){
+    public static String getMenu(Menu menu) {
         return Randoms.shuffle(menu.menus).get(0);
     }
 
@@ -38,6 +39,18 @@ public enum Menu {
                 .filter(menu -> isEqualCategory(menu, categoryNumber))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static List<String> createAllMenu() {
+        List<String> menuList = new ArrayList<>();
+        for (Menu item : Menu.values()) {
+            menuList.addAll(item.menus);
+        }
+        return menuList;
+    }
+
+    public static boolean hasMenu(String menu) {
+        return createAllMenu().contains(menu);
     }
 
     private static boolean isEqualCategory(Menu menu, int categoryNumber) {
