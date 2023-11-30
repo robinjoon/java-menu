2352fee6a77c8ba2d52cf76fa02d6a4a469609c7
DobbyMac <kimdobby@g.skku.edu>
2023-11-30 14:43:24 +0900

    feat- ErrorMessage: 사용자 출력 메세지 상수화

---
+++ src/main/java/menu/Message.java
@@ -0,0 +1,17 @@
+package menu;
+
+public class Message {
+
+    public final static String START_PROGRAM_PROMPT = "점심 메뉴 추천을 시작합니다.";
+
+    public final static String INPUT_COACH_PROMPT = "코치의 이름을 입력해 주세요. (, 로 구분)";
+
+    public final static String INPUT_CANT_EATABLE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+
+    public final static String OUTPUT_RESULT_PROMPT = "메뉴 추천 결과입니다.";
+
+    public final static String WEEKDAYS_PROMPT = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    public final static String CATEGORY_RECOMMEND_RESULT_FORMAT = "[ 카테고리 | %s | %s | %s | %s | %s ]";
+    public final static String MENU_RECOMMEND_RESULT_FORMAT = "[ %s | %s | %s | %s | %s | %s ]";
+    public final static String COMPLETE_RECOMMEND_MESSAGE = "추천을 완료했습니다.";
+}
