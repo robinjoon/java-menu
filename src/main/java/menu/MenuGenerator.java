f7d30e2516788d91958f66d53047873e76802f93
DobbyMac <kimdobby@g.skku.edu>
2023-11-30 16:45:57 +0900

    feat- MenuGenerator: 랜덤 카테고리와 메뉴 생성하는 기능 구현

---
+++ src/main/java/menu/MenuGenerator.java
@@ -0,0 +1,46 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import menu.MenuBoard.*;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class MenuGenerator {
+    private static List<Integer> randomCategoryNumbers;
+
+    public MenuGenerator() {
+        randomCategoryNumbers = generateCategoryNumber();
+    }
+
+    public List<String> generateMenusForEachCoach(Coach coach) {
+        List<String> randomMenus = new ArrayList<>();
+        int i = 0;
+        while (randomMenus.size() < 5) {
+            String menu = generateDailyMenu(MenuBoard.getMenus()
+                    .get(
+                            randomCategoryNumbers.get(i)-1
+                    ));
+            if (coach.notEatableFood().contains(menu) || randomMenus.contains(menu)) {
+                System.out.println(menu);
+                continue;
+            }
+            randomMenus.add(menu);
+            i++;
+        }
+        return randomMenus;
+    }
+
+    private static List<Integer> generateCategoryNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 5, 5);
+    }
+
+    private static String generateDailyMenu(List<String> menus) {
+        return Randoms.shuffle(menus).get(0);
+    }
+
+    public List<String> getRandomCategory() {
+        return randomCategoryNumbers.stream().map(num -> MenuBoard.getMenuCategory().get(num - 1)).toList();
+    }
+}
+
