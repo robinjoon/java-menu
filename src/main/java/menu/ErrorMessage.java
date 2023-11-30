09cbef671a25a9b5057e2e119e03048b676a9b4e
DobbyMac <kimdobby@g.skku.edu>
2023-11-30 14:43:06 +0900

    feat- ErrorMessage: 사용자 입력 오류 메세지 상수화

---
+++ src/main/java/menu/ErrorMessage.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class ErrorMessage {
+
+    public final static String COACH_NAME_LEN_ERROR = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다.";
+
+    public final static String COACH_NUMBER_ERROR = "[ERROR] 코치는 최소 2명, 최대 5명이여야 합니다.";
+
+}
