1c21f7f33626ee723e6f3ad69912065504e62d62
robinjoon <robin980108@naver.com>
2023-11-30 15:32:12 +0900

    refactor(프로그램 진행 기능) : 메서드 분리

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -12,28 +12,50 @@ import menu.util.RetryHelper;
 public class Application {
     public static void main(String[] args) {
         OutputManger.startGuide();
+        Coaches coaches = makeCoaches();
+        addHateMenus(coaches);
+        Recommend recommend = makeRecommend(coaches);
+        RecommendDTO recommendDTO = new RecommendDTO(recommend);
+        printRecommendResult(recommendDTO);
+    }
+
+    private static void addHateMenus(Coaches coaches) {
+        List<String> seperatedCoachNames = coaches.getCoachNames();
+        for (String coachName : seperatedCoachNames) {
+            addHateMenus(coaches, coachName);
+        }
+    }
+
+    private static void addHateMenus(Coaches coaches, String coachName) {
+        RetryHelper.retry(() -> {
+            OutputManger.inputHateNameGuide(coachName);
+            String hateMenus = InputManager.getString();
+            coaches.addHateMenus(coachName, hateMenus);
+            return Void.TYPE;
+        });
+    }
+
+    private static Coaches makeCoaches() {
         OutputManger.inputCoachNameGuide();
         Coaches coaches = RetryHelper.retry(() -> {
             String coachNames = InputManager.getString();
             return new Coaches(coachNames);
         });
-        List<String> seperatedCoachNames = coaches.getCoachNames();
-        for (String coachName : seperatedCoachNames) {
-            RetryHelper.retry(() -> {
-                OutputManger.inputHateNameGuide(coachName);
-                String hateMenus = InputManager.getString();
-                coaches.addHateMenus(coachName, hateMenus);
-                return Void.TYPE;
-            });
-        }
+        return coaches;
+    }
+
+    private static void printRecommendResult(RecommendDTO recommendDTO) {
+        OutputManger.recommendResultGuide();
+        OutputManger.recommendResult(recommendDTO);
+        OutputManger.endGuide();
+    }
+
+    private static Recommend makeRecommend(Coaches coaches) {
         Recommend recommend = new Recommend(coaches);
         for (int i = 0; i < 5; i++) {
             recommend.selectNextMenuType(Randoms.pickNumberInRange(1, 5));
             recommend.selectMenu();
         }
-        RecommendDTO recommendDTO = new RecommendDTO(recommend);
-        OutputManger.recommendResultGuide();
-        OutputManger.recommendResult(recommendDTO);
-        OutputManger.endGuide();
+        return recommend;
     }
 }
