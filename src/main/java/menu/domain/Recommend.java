e9307d55d94abf9016805bbc5698c6f1c8c9f2a7
robinjoon <robin980108@naver.com>
2023-11-30 16:03:40 +0900

    refactor(메뉴 추천 기능) : 메서드 분리

--- src/main/java/menu/domain/Recommend.java
+++ src/main/java/menu/domain/Recommend.java
@@ -24,7 +24,8 @@ public class Recommend {
         MenuType selectedMenuType = Arrays.stream(MenuType.values())
                 .filter(menuType -> menuType.getCode() == menuTypeNumber)
                 .findFirst().orElseThrow();
-        long beforeSelectedCount = menuTypes.stream().filter(menuType -> menuType == selectedMenuType)
+        long beforeSelectedCount = menuTypes.stream()
+                .filter(menuType -> menuType == selectedMenuType)
                 .count();
         if (beforeSelectedCount > 2) {
             throw new RuntimeException("다른 카테고리 설정 필요");
@@ -39,19 +40,33 @@ public class Recommend {
     private void selectMenu(Coach coach) {
         MenuType menuType = menuTypes.get(menuTypes.size() - 1);
         Map<MenuType, List<Menu>> menuBoard = MenuGenerator.menuBoard();
-        List<String> menus = menuBoard.get(menuType).stream().map(Menu::getName).collect(Collectors.toList());
-        List<String> hateMenus = coach.getHateMenus().stream().map(Menu::getName).collect(Collectors.toList());
-        List<String> preSelectedMenus = recommendMenus.get(coach).stream().map(Menu::getName)
-                .collect(Collectors.toList());
-        String selectedMenuName = Randoms.shuffle(menus).get(0);
-        while (preSelectedMenus.contains(selectedMenuName) || hateMenus.contains(selectedMenuName)) {
-            selectedMenuName = Randoms.shuffle(menus).get(0);
-        }
-        String finalSelectedMenuName = selectedMenuName;
+        String selectedMenuName = getSelectedMenuName(coach, menuType, menuBoard);
         Menu selectedMenu = menuBoard.get(menuType).stream()
-                .filter(menu -> menu.getName().equals(finalSelectedMenuName))
+                .filter(menu -> menu.getName().equals(selectedMenuName))
                 .findFirst().orElseThrow();
         recommendMenus.get(coach).add(selectedMenu);
+    }
+
+    private String getSelectedMenuName(Coach coach, MenuType menuType, Map<MenuType, List<Menu>> menuBoard) {
+        List<String> menus = menuBoard.get(menuType)
+                .stream()
+                .map(Menu::getName)
+                .collect(Collectors.toList());
+        String selectedMenuName = Randoms.shuffle(menus).get(0);
+        while (isInvalidSelectedMenuName(coach, selectedMenuName)) {
+            selectedMenuName = Randoms.shuffle(menus).get(0);
+        }
+        return selectedMenuName;
+    }
+
+    private boolean isInvalidSelectedMenuName(Coach coach, String selectedMenuName) {
+        List<String> hateMenus = coach.getHateMenus().stream()
+                .map(Menu::getName)
+                .toList();
+        List<String> preSelectedMenus = recommendMenus.get(coach).stream()
+                .map(Menu::getName)
+                .toList();
+        return preSelectedMenus.contains(selectedMenuName) || hateMenus.contains(selectedMenuName);
     }
 
     List<MenuType> getMenuTypes() {
